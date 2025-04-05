507b570d231c1e876b6ed2b22e7c38530f347afc
Joël Fah <69576717+Joel-Fah@users.noreply.github.com>
2025-04-05 09:10:04 +0100

    feat: added new project configurations with tailwind and production ready settings (#4)

--- src/backend/core/views.py
+++ src/backend/core/views.py
@@ -4,4 +4,4 @@ from django.views.generic import TemplateView
 # Create your views here.
 
 class HomeView(TemplateView):
-    template_name = "core/index.html"
+    template_name = 'core/index.html'
