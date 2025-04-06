12fe839a26911a7d2914c1901c27b3fb6e5b58d2
Egbemichel <149802747+Egbemichel@users.noreply.github.com>
2025-04-06 20:00:42 +0400

    feat: fork push test (#6)

--- src/frontend/app/page.tsx
+++ src/frontend/app/page.tsx
@@ -8,9 +8,6 @@ export default function Home() {
       <div className="text-xl font-sans">
         This text uses the custom archivo font!
       </div>
-      <div className="text-xl">
-        This text uses the custom archivo italic font!
-      </div>
     </nav>
   );
 }
