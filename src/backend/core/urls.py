f97f0867e03facc97ef10aff06fe334247137e6d
Radiance <nnamuradiance.gmail.com>
2025-05-27 13:33:41 +0100

    fit: reminders,notifications and resolutions(serializing)

--- src/backend/core/urls.py
+++ src/backend/core/urls.py
@@ -1,13 +1,19 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
+from django.contrib import admin
 from .views import HomeView, UserCreate, google_login_callback, UserDetailView, validate_google_token, google_logout, \
-    CategoryListCreateView, CategoryDetailView, UserListCreateView, UserDetailView
-
+    CategoryListCreateView, CategoryDetailView, UserListCreateView, UserDetailView, ReminderViewSet, \
+    NotificationViewSet, ResolutionListCreateView, ResolutionRetrieveUpdateDestroyView
 
 # Create your urls here
 
 app_name = 'core'
+router = DefaultRouter()
+router.register(r'reminders', ReminderViewSet, basename='reminder')
+router.register(r'notifications', NotificationViewSet, basename='notification')
+
+
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
@@ -27,5 +33,12 @@ urlpatterns = [
     path('api/users/', UserListCreateView.as_view(), name='user-list-create'),
     path('api/users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
 
-
+    #Reminders
+    path('admin/', admin.site.urls),
+    #notifications
+    path('', include(router.urls)),
+    #Resolution
+    path('api/resolutions/', ResolutionListCreateView.as_view(), name='resolution-list-create'),
+    path('api/resolutions/<int:pk>/', ResolutionRetrieveUpdateDestroyView.as_view(), name='resolution-detail'),
 ]
+urlpatterns += router.urls
