64c4c3c20149431964c686931d121d64eb24c00c
Joel-Fah <joelfah2003@gmail.com>
2025-06-03 09:39:18 +0100

    feat: added student profile update endpoint to api

--- src/backend/core/urls.py
+++ src/backend/core/urls.py
@@ -6,7 +6,7 @@ from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .views import ComplaintListCreateView, ComplaintDetailView
+from .views import ComplaintListCreateView, ComplaintDetailView, StudentProfileUpdateView
 from .views import HomeView, UserCreate, google_login_callback, validate_google_token, google_logout, \
     CategoryListCreateView, CategoryDetailView, UserListCreateView, UserDetailView, ReminderViewSet, \
     NotificationViewSet, ResolutionListCreateView, ResolutionRetrieveUpdateDestroyView, CourseListCreateView, \
@@ -64,6 +64,9 @@ urlpatterns = [
     # Users
     path('users/', UserListCreateView.as_view(), name='user-list-create'),
     path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
+
+    # student profile
+    path('users/students/profile/', StudentProfileUpdateView.as_view(), name='student-profile-update'),
 
     # notifications
     path('', include(router.urls)),
