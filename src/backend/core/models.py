f20c519612f906b9fc03c27cd380e11b0f41c53a
Joël Fah <69576717+Joel-Fah@users.noreply.github.com>
2025-06-08 01:33:01 +0100

    Update models.py

--- src/backend/core/models.py
+++ src/backend/core/models.py
@@ -19,7 +19,7 @@ class UserRole(str, Enum):
 
     @classmethod
     def choices(cls):
-        return [(tag.value, tag.value) for tag in cls]
+        return [(tag, tag.value) for tag in cls]
 
 
 class SemesterChoices(models.TextChoices):
