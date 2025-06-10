5c120f119d697b2825b6f3c210915b6545b3d5bc
Joël Fah <69576717+Joel-Fah@users.noreply.github.com>
2025-06-10 11:56:29 +0100

    fix requirements conflicts (#45)
    * feat: added project configurations
    
    * feat: added new project configurations with tailwind and production ready settings
    
    * chore: added new packages
    
    * fix: fixed link to adapter in settings
    
    * feat: added api config
    
    * feat: commit front end changes before pull
    
    * staging local env
    
    * feat: added google data to serializer
    
    * removed node modules on parent folder
    
    * stage commits after frontend api fix
    
    * feat: added logout feature
    
    * staging local files
    
    * staging local branch
    
    * fit:complaints,category
    
    * feat: added Tag and Attachment classes
    
    * feat: added User implementation and relationships
    
    * fit:endorsement, resolution, notification, reminder
    
    * fix: typos inside models
    
    * feat: updated models and added util functions
    
    * fit:CategoryListCreateView
    
    * feat: added category serialisers
    
    * feat: added CRUD operations for categories
    
    * fix: untrack unnecessary files, fixed projects settings
    
    * chore: ignored .pyc files in gitignore
    
    * fixed login flow, added models to admin dashboard, automated profiles check and creation upon login
    
    * fix: categories creation on app launch
    
    * fix login profile verification
    
    * feat: added courses endpoint
    
    * feat: added student profile update endpoint to api
    
    * fix: deadline calculation in complaint model
    
    * fix: fix profiles on users api
    
    * staging local branch
    
    * Added deployment configurations
    
    * feat: added cors to env
    
    * fix error in prod settings
    
    * fix boto3 storage
    
    * fix attempt for prod settings
    
    * added whitenoise to requirements
    
    * feat: added staticfiles collected and fixed static on prod
    
    ---------
    
    Co-authored-by: Radiance <nnamuradiance.gmail.com>

--- src/backend/ictucd/settings/base.py
+++ src/backend/ictucd/settings/base.py
@@ -119,14 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media/')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR / 'static/'),
     os.path.join(BASE_DIR / 'theme/static/'),
 ]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-MEDIA_ROOT = os.path.join(BASE_DIR / 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
