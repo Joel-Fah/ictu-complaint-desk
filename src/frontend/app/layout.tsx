1495cbc86b9ef94f7a20acac86962258badb9f49
TIM CHANTAL <timchantal13@gmail.com>
2025-06-25 13:17:11 +0100

    fix: addede mata tags

--- src/frontend/app/layout.tsx
+++ src/frontend/app/layout.tsx
@@ -5,26 +5,62 @@ import React from "react";
 import { Toaster } from "@/components/ui/sonner"
 import LayoutWrapper from "@/Usercomponents/LayoutWrapper";
 
+
 export const metadata: Metadata = {
-  title: "ICTU Complaint Desk",
-  description: "A platform for reporting and managing complaints",
+  title: "The ICTU Complaint Desk: A CRS for the ICT University",
+  description:
+    "Enter our Complaint Resolution System (CRS), The ICTU Complaint Desk – a simple, structured way for students to submit, track, and resolve issues with real-time updates.",
+  metadataBase: new URL("https://ictucd.live"),
+  openGraph: {
+    type: "website",
+    url: "https://ictucd.live",
+    title: "The ICTU Complaint Desk: A CRS for the ICT University",
+    description:
+      "Enter our Complaint Resolution System (CRS), The ICTU Complaint Desk – a simple, structured way for students to submit, track, and resolve issues with real-time updates.",
+    images: [
+      {
+        url: "https://raw.githubusercontent.com/Joel-Fah/images/refs/heads/main/ictucd-meta-img.png",
+        width: 1200,
+        height: 630,
+        alt: "ICTU Complaint Desk Preview Image",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "The ICTU Complaint Desk: A CRS for the ICT University",
+    description:
+      "Enter our Complaint Resolution System (CRS), The ICTU Complaint Desk – a simple, structured way for students to submit, track, and resolve issues with real-time updates.",
+    images: [
+      "https://raw.githubusercontent.com/Joel-Fah/images/refs/heads/main/ictucd-meta-img.png",
+    ],
+  },
+  themeColor: "#050041",
+  robots: {
+    index: true,
+    follow: true,
+  },
+  alternates: {
+    canonical: "https://ictucd.live",
+  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body>
-      <Toaster />
-       <UserProvider>
-           <LayoutWrapper>{children}</LayoutWrapper>
-       </UserProvider>
+        <Toaster />
+        <UserProvider>
+          <LayoutWrapper>{children}</LayoutWrapper>
+        </UserProvider>
       </body>
     </html>
   );
