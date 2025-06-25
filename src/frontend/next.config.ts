4e278c370c5f2541f5256deac2f7279ab20bc0f1
Joel-Fah <joelfah2003@gmail.com>
2025-06-25 23:04:24 +0100

    fix frontend images serving

--- src/frontend/next.config.ts
+++ src/frontend/next.config.ts
@@ -1,4 +1,4 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
     images: {
@@ -7,6 +7,11 @@ const nextConfig: NextConfig = {
                 protocol: 'https',
                 hostname: 'lh3.googleusercontent.com',
                 pathname: '/**', // Match all paths under this domain
+            },
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                pathname: '/**', // Match all paths under localhost
             },
         ],
     },
